4099e2e41b8c7ca0eece1bb701456378ca5b5687
Carlos Ruiz <caruizag@gmail.com>
2020-02-26 01:09:50 -0800

    [update] - Added login with provided cognito

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -7,9 +7,34 @@ export interface NavBarProps {
   userName: string;
 }
 
-export class NavBar extends React.Component<NavBarProps,{}>{
+export interface NavBarState {
+  isLoggedIn : boolean
+}
+
+
+export class NavBar extends React.Component<NavBarProps, NavBarState>{
+
+  constructor(props: NavBarProps) {
+    super(props);
+    this.state = { isLoggedIn: false,};
+
+  }
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
 
   render(){
+    const isLoggedIn = this.state.isLoggedIn;
+    let signIn;
+    if (isLoggedIn) {
+      signIn = <div>Signed in as: <a href="#login">{this.props.userName}</a></div>;
+    } else {
+      signIn = <div><a href="#login">Sign in</a></div>;
+    }
     return (
       <div>
         <Navbar bg="light" variant="light">
@@ -20,7 +45,7 @@ export class NavBar extends React.Component<NavBarProps,{}>{
           </Nav>
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              Signed in as: <a href="#login">{this.props.userName}</a>
+              {signIn}  
             </Navbar.Text>
           </Navbar.Collapse>
         </Navbar>
