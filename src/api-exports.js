8e8e5b7b399b72ade587ec1d3214706743533f9f
Carlos Ruiz <caruizag@gmail.com>
2021-06-27 17:49:11 -0700

    [update] - API Endpoint

--- src/api-exports.js
+++ src/api-exports.js
@@ -1,5 +1,5 @@
 const api_details = {
-  "base_endpoint": "https://3ws86cf176.execute-api.us-east-2.amazonaws.com/dev",
+  "base_endpoint": "https://uq5bl9t96b.execute-api.us-east-2.amazonaws.com/dev/",
   "base_endpoint_region": "us-east-2"
 };    
 
