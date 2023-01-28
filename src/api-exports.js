9785f4285ed50ad771cdfa9a0a6a4eaad02548ec
Carlos Ruiz <caruizag@gmail.com>
2023-01-27 21:17:39 -0800

    Update API endpoint - fix additional /

--- src/api-exports.js
+++ src/api-exports.js
@@ -1,7 +1,7 @@
 const api_details = {
   "dev_base_endpoint": "http://localhost:8080/20200201",
   "prod_endpoint" : "https://a0wowmhuia.execute-api.us-east-2.amazonaws.com/20200201",
-  "base_endpoint":  "https://a0wowmhuia.execute-api.us-east-2.amazonaws.com//20200201",
+  "base_endpoint":  "https://a0wowmhuia.execute-api.us-east-2.amazonaws.com/20200201",
   "base_endpoint_region": "us-east-2"
 };    
 
